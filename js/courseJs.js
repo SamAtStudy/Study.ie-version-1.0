44069b9e24dab5c3207d089060daba2738e946cf
AJStudy <51376706+AJStudy@users.noreply.github.com>
2019-06-09 17:35:46 +0100

    Update courseJs.js
    added additional functionality for modals

--- js/courseJs.js
+++ js/courseJs.js
@@ -5,3 +5,17 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.sidenav').sidenav();
 });
+
+$(document).ready(function(){
+    $('.modal').modal();
+    $('#loginBox').modal('open');
+});
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+});
+
+function createAccOption(){
+    $('.modal').modal('close');
+    $('#createAccBox').modal('open');
+}
