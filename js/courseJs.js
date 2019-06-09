5c0a050d5fc147206a56f4edb955cf3431aa3278
AJ-Scully <aaronscully2@gmail.com>
2019-06-09 22:02:51 +0100

    Sidenav opens on right side

--- js/courseJs.js
+++ js/courseJs.js
@@ -1,14 +1,19 @@
+//In Materilise, options are specified in name-value pairs.
+//var instances = M.Sidenav.init(elems, options);
+//would be for eg. var instances = M.Sidenav.init(elems, {edges:'left',btn:'true'});
+
 $(document).ready(function(){
     $('.parallax').parallax();
 });
 
 $(document).ready(function(){
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav({
+        edge:'right'
+    });
 });
 
 $(document).ready(function(){
     $('.modal').modal();
-    $('#loginBox').modal('open');
 });
 
 $(document).ready(function(){
