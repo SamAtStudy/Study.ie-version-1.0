72d335dfdbca3b0da600ffb331ce5026725ce66e
SamAtStudy <Sam@study.ie>
2019-06-10 09:36:12 +0100

    10/06/2019

--- js/courseJs.js
+++ js/courseJs.js
@@ -1,7 +1,35 @@
+//In Materilise, options are specified in name-value pairs.
+//var instances = M.Sidenav.init(elems, options);
+//would be for eg. var instances = M.Sidenav.init(elems, {edges:'left',btn:'true'});
+
 $(document).ready(function(){
     $('.parallax').parallax();
 });
 
 $(document).ready(function(){
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav({
+        edge:'right'
+    });
 });
+
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+});
+
+$(document).ready(function(){
+    $('select').formSelect();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.chips');
+    var instances = M.Chips.init(elems, options);
+});
+
+function createAccOption(){
+    $('.modal').modal('close');
+    $('#createAccBox').modal('open');
+}
