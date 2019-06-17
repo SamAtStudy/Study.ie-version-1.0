c42c0dde8f8770caf00e8af2d8f5e566f4fbac38
SamAtStudy <Sam@study.ie>
2019-06-17 17:39:33 +0100

    17/06/19 Sam

--- sqlConnectUsers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername='db1459480_SamAJ';
-$dbUsername='u1459480_study';
-$dbPassword='hT6RFxgd';
-$dbName='logindata';
-
-$connectUser=mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
