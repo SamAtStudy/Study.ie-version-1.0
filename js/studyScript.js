8a5da4c268b1663c1219c389a8011fadb1886e88
AJ-Scully <aaronscully2@gmail.com>
2019-07-11 18:31:07 +0100

    11/7/19 - Fixed Mobile Site Bugs

--- js/studyScript.js
+++ js/studyScript.js
@@ -57,36 +57,6 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-$(document).ready(function() {
-
-// Gets the video src from the data-src on each button
-
-    var $videoSrc;
-    $('.video-btn').click(function() {
-        $videoSrc = $(this).data( "src" );
-    });
-    console.log($videoSrc);
-
-
-
-// when the modal is opened autoplay it
-    $('#myModal').on('shown.bs.modal', function (e) {
-
-// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-        $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" );
-    })
-
-
-
-// stop playing the youtube video when I close the modal
-    $('#myModal').on('hide.bs.modal', function (e) {
-        // a poor man's stop video
-        $("#video").attr('src',$videoSrc);
-    })
-
-// document ready
-});
-
 $(document).ready(function(){
     $("#fbBtn").click(function(){
         window.location.href = "https://www.facebook.com/studyire/";
@@ -103,4 +73,33 @@ $(document).ready(function(){
     $("#lnBtn").click(function(){
         window.location.href = "https://www.linkedin.com/company/study-ie/";
     });
+
+    $('#learnOnlineFocus').on('click', function() {
+        //this scroll withour animations in chrome
+        $('#sectionThreeBox').get(0).scrollIntoView({
+            block: "start",
+            behavior: "smooth"
+        });
+    });
+
+    $('#learInPersonFocus').on('click', function() {
+        //this scroll withour animations in chrome
+        $('#sectionThreeRowTwo').get(0).scrollIntoView({
+            block: "start",
+            behavior: "smooth"
+        });
+    });
+
+    $('#trackLearningFocus').on('click', function() {
+        //this scroll withour animations in chrome
+        $('#sectionThreeRowThree').get(0).scrollIntoView({
+            block: "start",
+            behavior: "smooth"
+        });
+    });
+
 });
+
+
+
+
