dc9c853803ec5eed6d17f55b30feee270552e7e2
AJ-Scully <aaronscully2@gmail.com>
2019-08-30 20:51:06 +0100

    29/8/19 - Aaron Scully - Added styled group results page

--- groupResults.php
+++ groupResults.php
@@ -10,6 +10,24 @@ include 'header.php';
 
 <br>
 <div class = "container-fluid">
+    <div class="row justify-content-between mx-xl-5 mx-3 mt-xl-5 mt-3">
+        <div class="mx-md-4 mx-lg-3 mx-xl-4 mb-n3 mb-lg-n4 mb-xl-n5">
+            <h2 class="ml-3 mb-3" style="font-weight:700;">Groups</h2>
+        </div>
+        <div class="mx-md-4 mx-lg-3 mx-xl-4 mb-n3 mb-lg-n4 mb-xl-n5">
+            <div class="dropdown mr-3">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="sortDropdownMenuBtn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sort By
+                </button>
+                <div class="dropdown-menu gmd-2" aria-labelledby="sortDropdownMenuBtn" id="sortDropdownMenu">
+                    <a class="dropdown-item" href="#">Featured</a>
+                    <a class="dropdown-item" href="#">Most Members</a>
+                    <a class="dropdown-item" href="#">Least Members</a>
+                    <a class="dropdown-item" href="#">Newest</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row m-xl-5 m-3" id="groupResTileContainer">
     </div>
     <div class="row">
